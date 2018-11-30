e5ea8ed1567200c3c46ebab57ea3262fabe0ecd3
csoltero109 <chrissoltero109@gmail.com>
2018-11-29 22:22:36 -0800

    clean commit

--- app/src/main/java/com/example/rkjc/news_app_2/JobServiceSync.java
+++ app/src/main/java/com/example/rkjc/news_app_2/JobServiceSync.java
@@ -1,19 +1,14 @@
 package com.example.rkjc.news_app_2;
-import android.app.Application;
 import com.firebase.jobdispatcher.JobService;
 import com.firebase.jobdispatcher.JobParameters;
-import android.support.annotation.NonNull;
 import android.util.Log;
-import android.widget.Toast;
 
 public class JobServiceSync extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters job) {
-//        NewsItemRepository n = new NewsItemRepository(getApplication());
         new NewsItemRepository(getApplication()).databaseSyncSetter();
         Log.e("Sync","Working Properly");
-//        Toast.makeText(getApplicationContext(),"Syncing", Toast.LENGTH_SHORT).show();
         return true;
     }
 
